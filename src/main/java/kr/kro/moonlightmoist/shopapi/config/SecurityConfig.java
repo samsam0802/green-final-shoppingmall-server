a0c25cadaf3bc7f7ed9bfb3127ed69c9c51bfe53
LimByeongKuk <bye0414@naver.com>
2026-01-02 09:50:27 +0900

    CORS 설정 에 추가된 웹페이지 도메인 주소 허용 함
    - CORS 설정 에 추가된 웹페이지 도메인 주소 허용 함

--- src/main/java/kr/kro/moonlightmoist/shopapi/config/SecurityConfig.java
+++ src/main/java/kr/kro/moonlightmoist/shopapi/config/SecurityConfig.java
@@ -70,7 +70,11 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:5173",
                 "http://green-final-shoppingmall-react-ikg8.vercel.app",
-                "https://green-final-shoppingmall-react-ikg8.vercel.app"
+                "https://green-final-shoppingmall-react-ikg8.vercel.app",
+                "http://green-final-shoppingmall-react.vercel.app",
+                "https://green-final-shoppingmall-react.vercel.app",
+                "http://moisture-village.kro.kr",
+                "https://moisture-village.kro.kr"
                 ));
 //        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
         configuration.setAllowedHeaders(List.of("*")); // 수정 BY 병국
